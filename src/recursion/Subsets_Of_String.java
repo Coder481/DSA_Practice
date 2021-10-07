e4410b76ede485c8b517e3982ab3a1ebdd5e4e1f
Coder481 <hrithiksharma24092000@gmail.com>
2021-10-07 17:12:06 +0530

    Subsets Of String using recursion

---
+++ src/recursion/Subsets_Of_String.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class Subsets_Of_String {
+
+    public static void main(String[] args) {
+
+        String s = "abc";
+
+        solve(s,"");
+    }
+
+    private static void solve(String ip, String op) {
+        if (ip.length() == 0){
+            System.out.print(op+" ");
+            return;
+        }
+
+        String op1 = op;
+        String op2 = op;
+
+        op2 += ip.charAt(0);
+        ip = ip.substring(1);
+
+        solve(ip,op1);
+        solve(ip,op2);
+    }
+
+}
