5707cacf974ae02f2fc49e89902e183790314b55
Coder481 <hrithiksharma24092000@gmail.com>
2022-04-16 23:11:54 +0530

    BaseBall Game - LeetCode

---
+++ src/zzz_codingChallenges/leetcode/easy/BaseballGame.java
@@ -0,0 +1,45 @@
+package zzz_codingChallenges.leetcode.easy;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+
+    public static void main(String[] args) {
+        String[] ops = {"1"};
+        System.out.println(calPoints(ops));
+    }
+
+    private static int calPoints(String[] ops) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (String op : ops) {
+            switch (op) {
+                case "+": {
+                    int lastIndex = list.size() - 1;
+                    int secLastIndex = lastIndex - 1;
+                    list.add(list.get(lastIndex) + list.get(secLastIndex));
+                    break;
+                }
+                case "D": {
+                    int lastIndex = list.size() - 1;
+                    list.add(list.get(lastIndex) * 2);
+                    break;
+                }
+                case "C": {
+                    list.remove(list.size() - 1);
+                    break;
+                }
+                default: {
+                    list.add(Integer.parseInt(op));
+                }
+            }
+        }
+
+        return list.stream().mapToInt(i->i).sum();
+
+    }
+}
